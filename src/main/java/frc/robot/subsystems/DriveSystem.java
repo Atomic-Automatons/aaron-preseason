95ca40e61451f2bf9034da41b145cbf31c9328f8
adumbidiot <nathaniel.daniel23@outlook.com>
2018-09-21 14:59:23 -0700

    Initial commit

---
+++ src/main/java/frc/robot/subsystems/DriveSystem.java
@@ -0,0 +1,45 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.PWMVictorSPX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * Add your docs here.
+ */
+public class DriveSystem extends Subsystem {
+
+  static private DriveSystem instance = null;
+  static public DriveSystem getInstance(){
+    if(instance == null){
+      instance = new DriveSystem();
+    }
+    return instance;
+  }
+
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+  private DifferentialDrive drive;
+  public DriveSystem(){
+    PWMVictorSPX left = new PWMVictorSPX(0);
+    PWMVictorSPX right = new PWMVictorSPX(1);
+    drive = new DifferentialDrive(left, right);
+  }
+
+  public void arcadeDrive(double x, double y){
+    drive.arcadeDrive(x, y);
+  }
+
+  @Override
+  public void initDefaultCommand() {
+    // Set the default command for a subsystem here.
+    // setDefaultCommand(new MySpecialCommand());
+  }
+}
